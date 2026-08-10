37d4e011a35ae7135c0c3f02ce77f4db006a6634
MENKA-19 <manirohi108@gmail.com>
2026-08-10 22:02:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.96%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int st = 0,mid = 0,end = nums.size()-1;
+        while(mid <= end){
+
+            if(nums[mid] == 2){
+                swap(nums[mid],nums[end]);
+                end--;
+            }
+            else if(nums[mid] == 0){
+                swap(nums[st],nums[mid]);
+               st++;
+               mid++;
+            }
+            else{
+                mid++;
+            }
+        }
+    }
+};
